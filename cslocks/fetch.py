d1bf72e3c96621f98836387cf82793d03e11bd89
dawc <dawc@wwu.edu>
2018-09-21 13:36:28 -0700

    Add content-type header to post request to response_url

--- cslocks/fetch.py
+++ cslocks/fetch.py
@@ -7,6 +7,8 @@ def send_delayed_message(req_form):
     response_url = req_form['response_url']
     content = str(req_form)
 
+    # Slack requires the Content-type header be application/json
+    headers = {'Content-type': 'application/json'}
     payload_dict = {
         "text": "Original POST request",
         "attachments": [
@@ -16,4 +18,8 @@ def send_delayed_message(req_form):
         ]
     }
 
-    req = requests.post(response_url, data=json.dumps(payload_dict))
+    req = requests.post(
+            response_url,
+            data=json.dumps(payload_dict),
+            headers=headers
+        )
