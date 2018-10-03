127b3503f4a787d96211c7046f5ab6914a1a5b18
dawc <dawc@wwu.edu>
2018-10-03 15:45:12 -0700

    Add lookup functionality
    Formatted as any of the following:
    /lookup <key>
    /lookup <key> <table>
    /lookup find <key>
    /lookup find <key> <table>

--- teamdict/postgres.py
+++ teamdict/postgres.py
@@ -21,8 +21,7 @@ def create_table(form):
         None
     """
     with conn.cursor() as cur:
-        short_name, table_name = get_table_names(form)
-        print(f"{table_name} is a table: {is_table(table_name)}.")
+        short_name, table_name = get_table_names(form, 1)
         if is_table(table_name):
             send_delayed_message(
                     f'Table `{short_name}` exists.',
@@ -30,12 +29,12 @@ def create_table(form):
             return
 
         query = ('CREATE TABLE %s (' +
-                 'key VARCHAR UNIQUE NOT NULL PRIMARY KEY, ' +
+                 'key VARCHAR PRIMARY KEY, ' +
                  'value VARCHAR, ' +
                  'date_modified TIMESTAMPTZ NOT NULL DEFAULT now()' +
                  ');'
                  )
-        cur.execute(query, (psycopg2.extensions.AsIs(table_name),))
+        cur.execute(query, (as_is(table_name),))
         send_delayed_message(
                     f'Table `{short_name}` created!',
                     form['response_url'])
@@ -45,7 +44,7 @@ def create_table(form):
 #Slack api has "destructive buttons" for confirmation of destructive actions.
 def drop_table(form):
     with conn.cursor() as cur:
-        short_name, table_name = get_table_names(form)
+        short_name, table_name = get_table_names(form, 1)
         if not is_table(table_name):
             send_delayed_message(
                     f'No table named `{short_name}` exists.',
@@ -53,7 +52,7 @@ def drop_table(form):
             return
 
         query = 'DROP TABLE %s;'
-        cur.execute(query, (psycopg2.extensions.AsIs(table_name),))
+        cur.execute(query, (as_is(table_name),))
         send_delayed_message(
                     f'Table `{short_name}` dropped!',
                     form['response_url'])
@@ -61,7 +60,7 @@ def drop_table(form):
 
 def add_data(form):
     with conn.cursor() as cur:
-        short_name, table_name = get_table_names(form)
+        short_name, table_name = get_table_names(form, 1)
         if not is_table(table_name):
             send_delayed_message(
                     f'No table named `{short_name}` exists.',
@@ -71,10 +70,10 @@ def add_data(form):
         text = form['text'].split()
         key = text[2]
         value = text[3]
-        query = ('INSERT INTO %s (key, value)' +
+        query = ('INSERT INTO %s (key, value) ' +
                 'VALUES (%s, %s);')
         try:
-            cur.execute(query, (psycopg2.extensions.AsIs(table_name), key, value,))
+            cur.execute(query, (as_is(table_name), key, value,))
         except psycopg2.IntegrityError:
             #TODO: Ask user if they wish to update the value of duplicate key.
             send_delayed_message(
@@ -83,24 +82,24 @@ def add_data(form):
             return
 
         send_delayed_message(
-                f'Key `{key}` added to `{table_name}`',
+                f'Key `{key}` added to `{short_name}`',
                 form['response_url'])
         conn.commit()
 
 def delete_data(form):
     with conn.cursor() as cur:
-        short_name, table_name = get_table_names(form)
+        short_name, table_name = get_table_names(form, 1)
         if not is_table(table_name):
             send_delayed_message(
                     f'No table named `{short_name}` exists.',
                     form['response_url'])
             return
 
-        text = form['text'].split()
+        text = form['text'].lower().split()
         key = text[2]
         query = ('DELETE FROM %s ' +
                 'WHERE key = %s;')
-        cur.execute(query, (psycopg2.extensions.AsIs(table_name), key,))
+        cur.execute(query, (as_is(table_name), key,))
         item_existed = int(cur.statusmessage.split()[1]) > 0
         if item_existed:
             send_delayed_message(
@@ -112,18 +111,115 @@ def delete_data(form):
                     f'Key `{key}` was not found in `{short_name}`',
                     form['response_url'])
 
-
 def lookup(form):
-    return
+    text = form['text'].lower().split()
 
-def get_table_names(form):
+    if text[0] == 'find':
+        text.pop(0)
+
+    key = text[0]
+    values_found = [] #List of tuples, e.g. [(table, val), (table2, val2)]
+    if len(text) == 1: #Find all instances of 'key' in all tables
+        table_names = get_channel_tables(form)
+        for names in table_names:
+            short_name = names[0]
+            value = lookup_helper(form, key, names)
+            if len(value) > 0:
+                values_found.append((short_name, value))
+    elif len(text) == 2: #Find 'key' in the given table name
+        names = get_table_names(form, 1)
+        short_name = names[0]
+        value = lookup_helper(form, key, names)
+        values_found.append((short_name, value))
+    else:
+        send_delayed_message(
+                'Unable to execute command:',
+                form['response_url'],
+                attachments=f"form['command'] form['text']")
+        return
+
+    if len(values_found) == 0:
+        send_delayed_message(
+                f'No key found matching `{key}`.',
+                form['response_url'])
+    elif len(values_found) == 1:
+        send_delayed_message(
+                f'`{key}` found in {values_found[0][0]}:',
+                form['response_url'],
+                attachments=f'{key}: {values_found[0][1]}')
+    else:
+        values_str = ''
+        for value_table_pair in values_found:
+            table = value_table_pair[0]
+            value = value_table_pair[1]
+            values_str += f'{table}: {value}\n'
+        send_delayed_message(
+                f'`{key}` found in {len(values_found)} tables:',
+                form['response_url'],
+                attachments=values_str)
+
+
+#TODO: document that this function does the query and returns the result
+# and that the lookup() function sends a message to slack once it gets all the
+# queries done.
+def lookup_helper(form, key, table_names):
+    result = []
+    with conn.cursor() as cur:
+
+        short_name, table_name = table_names
+        if not is_table(table_name):
+            send_delayed_message(
+                    f'No table named `{short_name}` exists.',
+                    form['response_url'])
+            return
+
+        query = ('SELECT value FROM %s ' +
+                'WHERE key = %s;')
+        cur.execute(query, (as_is(table_name), key,))
+        result = cur.fetchall()
+
+    if len(result) > 0:
+        return result[0][0] #Extract value from a tuple in an array
+    else:
+        return result
+
+#######################
+#  UTILITY FUNCTIONS  #
+######################
+
+def get_table_names(form, tn_index):
     text = form['text'].lower().split()
     team_domain = form['team_domain']
     channel_id = form['channel_id']
-    short_name = text[1]
+    short_name = text[tn_index]
     table_name = f'{team_domain}_{channel_id}_{short_name}'
 
     return (short_name, table_name)
+
+def add_short_name(table_name):
+    name = table_name.split('_')
+    name.pop(0)
+    name.pop(0)
+    short_name = '_'.join(name)
+
+    return (short_name, table_name)
+
+def get_channel_tables(form):
+    tables = []
+    with conn.cursor() as cur:
+        team_domain = form['team_domain']
+        channel_id = form ['channel_id']
+        table_prefix = f"^{team_domain}_{channel_id}"
+
+        query = ('SELECT table_name ' +
+                'FROM information_schema.tables ' +
+                'WHERE table_name ~ %s;')
+        cur.execute(query, (table_prefix,))
+        tables = [table[0] for table in cur.fetchall()]
+        #TODO: Figure out how to break apart what the cursor fetched
+        short_long_names = [add_short_name(table) for table in tables]
+
+    return short_long_names
 
 def is_table(table_name):
     """
@@ -151,3 +247,5 @@ def is_table(table_name):
     else:
         return False
 
+def as_is(table_name):
+    return psycopg2.extensions.AsIs(table_name)
--- teamdict/util.py
+++ teamdict/util.py
@@ -17,14 +17,14 @@ def triage_command(request):
     response_url = form['response_url']
     slash_command = form['command']
 
-    if not is_valid_request(request):
-        message = "Access denied!"
-        send_delayed_message(message, response_url)
-        return
+#    if not is_valid_request(request):
+#        message = "Access denied!"
+#        send_delayed_message(message, response_url)
+#        return
 
     text = form['text'].lower().split()
 
-    if len(text) < 2:
+    if len(text) == 0:
         send_help(slash_command, response_url)
         return
     else:
@@ -32,16 +32,19 @@ def triage_command(request):
 
     #TODO: Parameter checking before passing the buck to the database handler.
     if command == 'help' or command == '':
+        #TODO: Allow for help on specific commands /lookup help <cmd>
         send_help(slash_command, response_url)
     elif command == 'create':
         db.create_table(form)
+    elif command == 'drop':
+        db.drop_table(form)
     elif command == 'add' or command == 'populate':
         db.add_data(form)
     elif command == 'delete':
         db.delete_data(form)
-    elif command == 'drop':
-        db.drop_table(form)
-    elif db.is_table(command):
+    elif command == 'find':
+        db.lookup(form)
+    elif len(text) == 1:
         db.lookup(form)
     else:
         send_help(slash_command, response_url, message='Command not found.')
